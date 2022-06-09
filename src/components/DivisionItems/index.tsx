64481bd105aaf13b9d897ea7d1c687618bf8646b
caiocof <caiooliveira3652@outlook.com>
2022-06-09 13:09:25 -0300

    feat: create the division component

---
+++ src/components/DivisionItems/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css'
+
+interface DivisionItemsProps {
+    mainColor: string;
+    completed: number
+}
+
+export const DivisionItems = ({ mainColor, completed }: DivisionItemsProps) => {
+    return (
+        <div className='containerStyles'>
+            <div className='fillerStyles' style={{ backgroundColor: mainColor || '#FB9400', width: `${completed}%` }}>
+                <span className='labelStyles' style={{ color: 'rgba(0,0,0,0)' }}>{`${completed}`}</span>
+            </div>
+        </div>
+    );
+}
